e70757c51534703e4292bd8f54810ceb393bc1fc
sumit2898 <kumarsumit272006gmail.com>
2026-05-21 01:35:40 +0530

    closer

--- 09advance/02/10gettersetter.js
+++ 09advance/02/10gettersetter.js
@@ -12,7 +12,7 @@ class User {
     }
 
     get password(){
-        return `${this._password}hitesh`
+        return `${this._password}sumit`
     }
 
     set password(value){
@@ -20,5 +20,55 @@ class User {
     }
 }
 
-const hitesh = new User("h@hitesh.ai", "abc")
+const hitesh = new User("s@sumit.ai", "abc")
 console.log(hitesh.email);
+
+//
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const kumar = new User("kumar@k.com", "kumar")
+
+console.log(kumar.email);
+
+
+
+//object using getters and setters
+
+const User = {
+    _email: 's@sk.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const kumar = Object.create(User)
+console.log(kumar.email);
